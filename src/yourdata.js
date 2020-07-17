481fec3f014298ba56c2eb2ea979ae4da2c1169e
raazatul7 <raazatul7@gmail.com>
2020-07-17 23:09:22 +0530

    initial

---
+++ src/yourdata.js
@@ -0,0 +1,79 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+  name: "atulrajgupta",
+  headerTagline: [
+    //Line 1 For Header
+    "Hi 👋 You found me! I am Atul.",
+    //Line 2 For Header
+    "Software Dev ",
+    //Line 3 For Header
+    "from India",
+  ],
+  //Contact Email
+  contactEmail: "raazatul7@gmail.com",
+  // Add Your About Text Here
+  abouttext:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  //Change This To Hide The Image of About Section (True Or False)
+  ShowAboutImage: true, //true or false (Change Here)
+  // Change Projects Here
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Project One", //Project Title - Add Your Project Title Here
+      service: "UI/UX Design", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "#",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Two",
+      service: "Website Development",
+      imageSrc:
+        "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url: "#",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Three",
+      service: "Web App",
+      imageSrc:
+        "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url: "#",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Four",
+      service: "Branding",
+      imageSrc:
+        "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url: "#",
+    },
+
+    /*
+
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 5,
+                title: 'Project Five',
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+  ],
+  social: [
+    // Add Or Remove The Link Accordingly
+    { name: "Github", url: "https://github.com/raazatul7" },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/raazatul7",
+    },
+  ],
+};
