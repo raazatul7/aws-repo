6436585f25f4ce9be5debe412eeb1642b1808d4f
raazatul7 <raazatul7@gmail.com>
2020-07-17 23:54:01 +0530

    image added

--- src/yourdata.js
+++ src/yourdata.js
@@ -16,7 +16,7 @@ export default {
   abouttext:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   aboutImage:
-    "https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    "https://lh3.googleusercontent.com/LATA67ekksy8SGQxBo5GG-iUnMoXltFlgW11pi1eyT9vZlnZP-DUM0QfsmYoE-xNHrHFcMvsSKYYJa3aAGy6ZIaQIVxJ3xNGHN4a8HLXo6LUzM-nb2OG3EsSyI1uZ-HSfYX4Gg7ygcWQ4fFlLqnYXdPo9kkWlI5wL5NtcMC5Rn9c251I2zW5cGo8UB-gbOD156p_P1NQ01EQmHQQ018x27ksxE_IoT4PpAZt3OeNY8DedV0Vn-KTqhb_H2MxBaTxVQZ1qcGcvYaKESPs8zMttQqvZupnDwGpe0m4m8Xbb3KOpGMn5BTw7nH78CgEcW6DzHLJlSP4Od15nle1ZW5jT88kD-gz79jNrlm9FIOBIp8YFawM5hLhobK46VZcLunn-abbqfCXVeyeNHvWXUUKC2KkB_SDZObd3V_Jthu5UHa7q0Tpe_jNY2TZWWviaIZgBuz4aQW8zlZfhkLgOgw_toV6pf2u2E23x1nXIzm28O45nqZKEPxJJL4XPymVsqytIrQIzaOKi2JWtiVYgDT7t95UGCevdyifs52e7mHoXnvZqzSIFOzhKcVN_hSSOJnQsXAEt4f0Bbw_RHeqXrfE1lxc4gQlRSSIcINllSQd_MUFN4hujIombpkskT5VwX-J0khk2WjpgHfDB_HuPqJjFJt6z8x_OSDSMFzjW9T55ZJPd6YZkhWMNzOKiliAyZM=w467-h535-no?authuser=0",
   //Change This To Hide The Image of About Section (True Or False)
   ShowAboutImage: true, //true or false (Change Here)
   // Change Projects Here
