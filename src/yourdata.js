dfe93160e5c80a29397df4e1c6a47e65adeac128
raazatul7 <raazatul7@gmail.com>
2020-07-19 18:16:04 +0530

    proj added

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,3 +1,9 @@
+import aboutImage from "./assets/aboutImage.png";
+import proj_mySamaj from "./assets/mysamaj.jpeg";
+import proj_loyalTech from "./assets/loyalTech.jpeg";
+import proj_spiri from "./assets/spiri.jpeg";
+import proj_rememberLikes from "./assets/rememberLikes.jpeg";
+import proj_soon from "./assets/soon.jpeg";
 export default {
   //(Please Do Not Remove The comma(,) after every variable)
   //Change The Website Template
@@ -15,65 +21,60 @@ export default {
   // Add Your About Text Here
   abouttext:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  aboutImage:
-    "https://lh3.googleusercontent.com/LATA67ekksy8SGQxBo5GG-iUnMoXltFlgW11pi1eyT9vZlnZP-DUM0QfsmYoE-xNHrHFcMvsSKYYJa3aAGy6ZIaQIVxJ3xNGHN4a8HLXo6LUzM-nb2OG3EsSyI1uZ-HSfYX4Gg7ygcWQ4fFlLqnYXdPo9kkWlI5wL5NtcMC5Rn9c251I2zW5cGo8UB-gbOD156p_P1NQ01EQmHQQ018x27ksxE_IoT4PpAZt3OeNY8DedV0Vn-KTqhb_H2MxBaTxVQZ1qcGcvYaKESPs8zMttQqvZupnDwGpe0m4m8Xbb3KOpGMn5BTw7nH78CgEcW6DzHLJlSP4Od15nle1ZW5jT88kD-gz79jNrlm9FIOBIp8YFawM5hLhobK46VZcLunn-abbqfCXVeyeNHvWXUUKC2KkB_SDZObd3V_Jthu5UHa7q0Tpe_jNY2TZWWviaIZgBuz4aQW8zlZfhkLgOgw_toV6pf2u2E23x1nXIzm28O45nqZKEPxJJL4XPymVsqytIrQIzaOKi2JWtiVYgDT7t95UGCevdyifs52e7mHoXnvZqzSIFOzhKcVN_hSSOJnQsXAEt4f0Bbw_RHeqXrfE1lxc4gQlRSSIcINllSQd_MUFN4hujIombpkskT5VwX-J0khk2WjpgHfDB_HuPqJjFJt6z8x_OSDSMFzjW9T55ZJPd6YZkhWMNzOKiliAyZM=w467-h535-no?authuser=0",
+  aboutImage: aboutImage,
   //Change This To Hide The Image of About Section (True Or False)
   ShowAboutImage: true, //true or false (Change Here)
   // Change Projects Here
   projects: [
     {
-      id: 1, //DO NOT CHANGE THIS (Please)😅
-      title: "Project One", //Project Title - Add Your Project Title Here
-      service: "UI/UX Design", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-      //Project URL - Add Your Project Url Here
+      id: 1,
+      title: "Project One",
+      service: "Mobile Application",
+      imageSrc: proj_mySamaj,
       url: "#",
     },
     {
-      id: 2, //DO NOT CHANGE THIS (Please)😅
-      title: "Project Two",
-      service: "Website Development",
-      imageSrc:
-        "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      id: 2,
+      title: "Loyal Techs",
+      service: "Mobile Application",
+      imageSrc: proj_loyalTech,
       url: "#",
     },
     {
-      id: 3, //DO NOT CHANGE THIS (Please)😅
-      title: "Project Three",
-      service: "Web App",
-      imageSrc:
-        "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      id: 3,
+      title: "Soon",
+      service: "Mobile Application",
+      imageSrc: proj_soon,
       url: "#",
     },
     {
-      id: 4, //DO NOT CHANGE THIS (Please)😅
-      title: "Project Four",
-      service: "Branding",
-      imageSrc:
-        "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      id: 4,
+      title: "Spiri",
+      service: "Mobile Application",
+      imageSrc: proj_spiri,
       url: "#",
     },
-
-    /*
-
-                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
-                ,{
-                id: 5,
-                title: 'Project Five',
-                service: 'Something Amazing',
-                imageSrc: "",
-                url: ''
-            }
-                */
+    {
+      id: 5,
+      title: "Remember Likes",
+      service: "Mobile Application",
+      imageSrc: proj_rememberLikes,
+      url: "#",
+    },
   ],
   social: [
     // Add Or Remove The Link Accordingly
-    { name: "Github", url: "https://github.com/raazatul7" },
+    {
+      name: "Github",
+      url: "https://github.com/raazatul7",
+    },
     {
       name: "Instagram",
       url: "https://www.instagram.com/raazatul7",
+    },
+    {
+      name: "LinkedIn",
+      url: "linkedin.com/in/atul-raj-gupta-3a03a921",
     },
   ],
 };
