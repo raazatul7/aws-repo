6b7e999a1b41336d1cc0758d55b7709a62111332
raazatul7 <raazatul7@gmail.com>
2020-07-19 18:23:07 +0530

    about added

--- src/yourdata.js
+++ src/yourdata.js
@@ -20,7 +20,7 @@ export default {
   contactEmail: "raazatul7@gmail.com",
   // Add Your About Text Here
   abouttext:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Atul is amazing tech geek, having good understanding and problem solving skills. He is one of the best mobile app developers in India. He love to keep himself updated with new tech stack and regularly spread his knowledge through writing cool articles.",
   aboutImage: aboutImage,
   //Change This To Hide The Image of About Section (True Or False)
   ShowAboutImage: true, //true or false (Change Here)
